4ff935d5fdcf9dbb28e471a0ccc8c656d92a80a2
Sorikmir <sergiimyr@gmail.com>
2024-12-29 00:17:21 -0500

    navigation+routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CatCardComponent } from './cat-card/cat-card.component';
+import { CatsListComponent } from './cats-list/cats-list.component';
+import { HomeComponent } from './home/home.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'homepage',
+        component: HomeComponent,
+    },
+    {
+        path: 'cat-profile',
+        component: CatCardComponent,
+    },
+    {
+        path: 'cats-list',
+        component: CatsListComponent,
+    },
+    {
+        path: 'contacts',
+        component: ContactsComponent,
+    },
+    {
+        path: '**',
+        component: NotFoundComponent,
+    }
+];
---
+++ src/app/cats-list/cats-list.component.ts
@@ -0,0 +1,104 @@
+import { Component } from '@angular/core';
+import { CatCardComponent } from '../cat-card/cat-card.component';
+import { NgForOf } from '@angular/common';
+import { Cat } from '../app.component';
+
+@Component({
+  selector: 'app-cats-list',
+  imports: [CatCardComponent, NgForOf],
+  templateUrl: './cats-list.component.html',
+  styleUrl: './cats-list.component.scss',
+  standalone: true
+})
+export class CatsListComponent {
+  cats: Cat[] = [
+    {
+    breed: "Abyssinian",
+    name: "Marsik",
+    image: {
+    src: "images/1st-cat.jpg",
+    
+    alt: "Photo of an Abyssinian cat"
+    },
+    content: "The Abyssinian is easy to care for, and a joy to have in your home. They are incredibly loyal, intelligent, and deeply enjoy being part of a family.",
+    actions: ["LIKE", "SHARE"]
+    },
+    {
+    breed: "Bengal",
+    name: "Marsik",
+    image: {
+    src: "images/2nd-cat.jpg",
+    
+    alt: "Photo of a Bengal cat"
+    
+    },
+    content: "The Bengal is a sleek and athletic cat, reminiscent of their wild ancestors. They are playful, energetic, and love to climb.",
+    actions: ["LIKE", "SHARE"]
+    },
+    {
+    breed: "Persian",
+    name: "Marsik",
+    image: {
+    src: "images/3d-cat.jpg",
+    
+    alt: "Photo of a Persian cat"
+    },
+    content: "Persian cats are known for their long, luxurious coats and their sweet, gentle personalities. They love quiet and serene environments.",
+    actions: ["LIKE", "SHARE"]
+    },
+    {
+    breed: "Sphynx",
+    name: "Marsik",
+    image: {
+    src: "images/4th-cat.jpg",
+    
+    alt: "Photo of a Sphynx cat"
+    },
+    content: "The Sphynx is a hairless breed of cat known for its extroverted behavior. They are highly social and enjoy being the center of attention.",
+    actions: ["LIKE", "SHARE"]
+    },
+    { 
+    breed: "Maine Coon",
+    name: "Marsik",
+    image: {
+    src: "images/5th-cat.jpg",
+    alt: "Photo of a Maine Coon cat"
+    },
+    content: "Maine Coons are large, gentle giants with a friendly and affectionate personality. They are known for their intelli.",
+    actions: ["LIKE", "SHARE"]
+},
+{
+breed: "Sphynx",
+    name: "Marsik",
+    image: {
+    src: "images/6 th-cat.jpg",
+    
+    alt: "Photo of a Sphynx cat"
+    },
+    content: "The Sphynx is a hairless breed of cat known for its extroverted behavior. They are highly social and enjoy being the center of attention.",
+    actions: ["LIKE", "SHARE"]
+    },
+    {
+      breed: "Sphynx",
+          name: "Marsik",
+          image: {
+          src: "images/7th-cat.jpg",
+          
+          alt: "Photo of a Sphynx cat"
+          },
+          content: "The Sphynx is a hairless breed of cat known for its extroverted behavior. They are highly social and enjoy being the center of attention",
+          actions: ["LIKE", "SHARE"]
+          },
+          {
+            breed: "Sphynx",
+                name: "Marsik",
+                image: {
+                src: "images/8th-cat.jpg",
+                
+                alt: "Photo of a Sphynx cat"
+                },
+                content: "The Sphynx is a hairless breed of cat known for its extroverted behavior. They are highly social and enjoy being the center of attention.",
+                actions: ["LIKE", "SHARE"]
+                }]
+
+}
